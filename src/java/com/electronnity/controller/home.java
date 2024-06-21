7de0edfe5ad4518600ed2fb51b144500f0066053
zip-sapp <aaronsaplala7@gmail.com>
2024-06-21 20:01:23 +0800

    revert

--- src/java/com/electronnity/controller/home.java
+++ src/java/com/electronnity/controller/home.java
@@ -36,7 +36,7 @@ public class home extends HttpServlet {
     private void viewhome (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         RequestDispatcher hm = getServletContext().getRequestDispatcher(
-                "/shop.jsp");
+                "/home.jsp");
         hm.forward(request, response);
     }
 }
